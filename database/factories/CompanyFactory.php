aeaa26942d855b1d0f4edabdc0d23a27f12ba895
Alp Toker <tokeralp@gmail.com>
2025-02-05 14:14:57 +0300

    createFakeInfo

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Company;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class CompanyFactory extends Factory
+{
+    protected $model = Company::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->company(),
+            'email' => fake()->unique()->companyEmail(),
+            'logo' => fake()->imageUrl(200, 200, 'business'),
+            'website' => fake()->url(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Company;
+use App\Models\Employee;
+use Database\Seeders\AdminUserSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,8 +14,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Create admin user
         $this->call([
             AdminUserSeeder::class,
         ]);
+
+        // Create 50 companies
+        Company::factory(50)->create()->each(function ($company) {
+            // For each company, create between 1 and 3 employees
+            Employee::factory(fake()->numberBetween(1, 3))->create([
+                'company_id' => $company->id
+            ]);
+        });
     }
 }
